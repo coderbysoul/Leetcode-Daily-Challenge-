dfb265096221f642b74766a6bbfad0eee6b10277
Arjya <arjyabhattacharjee5@gmail.com>
2023-02-01 21:35:45 +0530

    Added files

---
+++ 32_greatestcommondivisorofstrings.cpp
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/greatest-common-divisor-of-strings/description/
+
+class Solution {
+    public:
+    string gcdOfStrings(string str1, string str2)
+    {
+        return (str1+str2 == str2+str1)?
+        str1.substr(0,gcd(size(str1),size(str2))): " ";
+    }
+};
