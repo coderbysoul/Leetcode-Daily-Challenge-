8307128fc342a4dbbc5d83eaa932c638342d2dae
Arjya <arjyabhattacharjee5@gmail.com>
2023-03-17 12:31:25 +0530

    Added files

---
+++ 76_implementtrie(prefixtree).cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/implement-trie-prefix-tree/description/
+
+class Trie {
+public:
+
+unordered_map<string,int>mpp,mp;
+    Trie() {
+        
+    }
+    
+    void insert(string word) {
+        mpp[word]++;
+        string s;
+        for(auto it : word)
+        {
+            s.push_back(it);
+            if(mp[s]==0) mp[s]++;
+        }
+    }
+    
+    bool search(string word) {
+        if(mpp[word]) return true;
+        return false;
+    }
+    
+    bool startsWith(string prefix) {
+        if(mp[prefix]) return true;
+        return false;
+    }
+};
